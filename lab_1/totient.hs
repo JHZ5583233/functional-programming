bff0ce3da149b4acf32bbaf319974d65862b9832
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-11-19 11:13:42 +0100

    trying to fix

--- lab_1/totient.hs
+++ lab_1/totient.hs
@@ -1,6 +1,5 @@
 totient :: Integer -> Integer
 totient n
-    | n == 2 = 1
     | n > 1 = result - div result n
     | otherwise = result
     where
@@ -8,11 +7,11 @@ totient n
 
 firstLayerOfTotient :: Integer -> Integer -> Integer -> Integer
 firstLayerOfTotient n p r
-    | p^2 < n = r
-    | mod n p == 0 = firstLayerOfTotient (reduce n p) (p - 1) (r - div r p)
+    | p^2 > n = r
+    | mod n p == 0 = firstLayerOfTotient (reduce n p) (p + 1) (r - div r p)
     | otherwise = firstLayerOfTotient n (p + 1) r
 
 reduce :: Integer -> Integer -> Integer
 reduce n p
-    | mod n p == 0 = reduce (mod n p) p
+    | mod n p == 0 = reduce (div n p) p
     | otherwise = n
