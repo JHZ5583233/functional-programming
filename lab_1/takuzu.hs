014952dba107f130bb279fb1e69893373f0e8a43
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-11-20 13:40:13 +0100

    finished problem

--- lab_1/takuzu.hs
+++ lab_1/takuzu.hs
@@ -1,1 +1,18 @@
+import Data.List (sort)
+
 takuzuStrings :: Int -> [String]
+takuzuStrings n = sort [x | x <- oneHelper n ["0", "00"] ++ zeroHelper n ["1", "11"], length x == n]
+
+oneHelper :: Int -> [String] -> [String]
+oneHelper n xs
+    | new == xs = xs
+    | otherwise = zeroHelper n new
+    where
+        new = [x ++ h | x <- xs, h <- ["1", "11"], length (h ++ x) <= n] ++ [p | p <- xs, length p == n]
+
+zeroHelper :: Int -> [String] -> [String]
+zeroHelper n xs
+    | new == xs = xs
+    | otherwise = oneHelper n new
+    where
+        new = [x ++ h | x <- xs, h <- ["0", "00"], length (h ++ x) <= n] ++ [p | p <- xs, length p == n]
