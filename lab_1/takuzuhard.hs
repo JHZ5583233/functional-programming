e16b985e09acecdcee2ef2fd685701ebae93ce27
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-11-26 11:10:23 +0100

    working on

--- lab_1/takuzuhard.hs
+++ lab_1/takuzuhard.hs
@@ -1,34 +1,43 @@
+import Data.List (sort)
 isCorrectTakuzu :: [String] -> Bool
 isCorrectTakuzu = all isCorrect
 
 isCorrect :: String -> Bool
-isCorrect s
-    | h == '1' = oneEater s
-    | h == '0' = zeroEater s
-    | otherwise = oneEater s /= zeroEater s
-        where
-            h = head s
-
-oneEater :: String -> Bool
-oneEater s
-    | null s = True
-    | h == '1' = oneEater xs
-    | xh == '1' = zeroEater xs
-    | otherwise = zeroEater xs /= zeroEater xxs
+isCorrect s = length (unqiue (concat2 [[y| y <- t, possible s y] | x <- s])) == length s
     where
-        h = head s
-        xs = tail s
-        xh = head xs
-        xxs = tail xs
+        t = takuzuStrings (length s)
 
-zeroEater :: String -> Bool
-zeroEater s
-    | null s = True
-    | h == '0' = zeroEater xs
-    | xh == '0' = zeroEater xs
-    | otherwise = oneEater xs /= oneEater xxs
+concat2 :: [[a]] -> [a]
+concat2 s
+    | null s = []
+    | otherwise = head s ++ concat2 (tail s)
+
+unqiue :: [a] -> [a]
+unqiue s = [x| x <- s, elemNum x s == 1]
+
+possible :: String -> String -> Bool
+possible xs ys
+    | xs == ys = True
+    | head xs == '.' = possible (tail xs) (tail ys)
+    | head xs == head ys = possible (tail xs) (tail ys)
+    | otherwise = False
+
+elemNum ::forall a. Eq a => a -> [a] -> Integer
+elemNum x s = sum [1 | y <- s, y == x]
+
+takuzuStrings :: Int -> [String]
+takuzuStrings n = sort [x | x <- oneHelper n ["0", "00"] ++ zeroHelper n ["1", "11"],length x == n, elemNum '1' x == elemNum '0' x]
+
+oneHelper :: Int -> [String] -> [String]
+oneHelper n xs
+    | new == xs = xs
+    | otherwise = zeroHelper n new
     where
-        h = head s
-        xs = tail s
-        xh = head xs
-        xxs = tail xs
+        new = [x ++ h | x <- xs, h <- ["1", "11"], length (h ++ x) <= n] ++ [p | p <- xs, length p == n]
+
+zeroHelper :: Int -> [String] -> [String]
+zeroHelper n xs
+    | new == xs = xs
+    | otherwise = oneHelper n new
+    where
+        new = [x ++ h | x <- xs, h <- ["0", "00"], length (h ++ x) <= n] ++ [p | p <- xs, length p == n]
