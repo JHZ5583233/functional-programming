2e302e91354808116b59a75b7a2f6598c088477a
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-04 10:42:05 +0100

    hkjafhd

--- lab_2/primsiev.hs
+++ lab_2/primsiev.hs
@@ -1,5 +1,2 @@
-primes::[Integer]
-primes = 2 : [(2*x) + 1| x <- [1..], null ([i + j + (2*j*i)| i <- [1.. floor (sqrt (fromIntegral  x))], j <- [i..x], i + j + (2*j*i) <= x, i + j + (2*j*i) == x])]
-
-noNum :: [Integer]
-noNum = [i + j + (2*j*i)| i <- [1..], j <- [i..]]
+    primes::[Integer]
+    primes = 2 : [(2*x) + 1| x <- [1..], x `notElem` ([i + j + (2*j*i)| i <- [1.. floor (sqrt (fromIntegral  x))], j <- [i..x], i + j + (2*j*i) <= x])]
