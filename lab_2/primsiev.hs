4c1985aeda198f690e6639ea15735888cda2e6b1
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-04 10:08:48 +0100

    trying something stupid

--- lab_2/primsiev.hs
+++ lab_2/primsiev.hs
@@ -1,2 +1,5 @@
 primes::[Integer]
-primes = [1..]
+primes = 2 : [(2*x) + 1| x <- [1..], null ([i + j + (2*j*i)| i <- [1.. floor (sqrt (fromIntegral  x))], j <- [i..x], i + j + (2*j*i) <= x, i + j + (2*j*i) == x])]
+
+noNum :: [Integer]
+noNum = [i + j + (2*j*i)| i <- [1..], j <- [i..]]
