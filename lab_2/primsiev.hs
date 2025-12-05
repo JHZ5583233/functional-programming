9256617a3fff80a7cb40921b1c754358477c418a
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-05 13:49:00 +0100

    ewhwekfjkew

--- lab_2/primsiev.hs
+++ lab_2/primsiev.hs
@@ -1,2 +1,9 @@
-    primes::[Integer]
-    primes = 2 : [(2*x) + 1| x <- [1..], x `notElem` ([i + j + (2*j*i)| i <- [1.. floor (sqrt (fromIntegral  x))], j <- [i..x], i + j + (2*j*i) <= x])]
+primes::[Integer]
+primes = 2 : [(2*x) + 1| x <- [1..], x `notElem` [i + j + (2*j*i)| i <- [1..x], j <- [i..x]]]
+
+noList :: Integer -> [Integer]
+noList x
+    | x == 1 = [i + x + (2*x*i)| i <- [1..x]]
+    | otherwise = ([i + x + (2*x*i)| i <- [1..x]]) ++ lastl
+        where
+            lastl = noList (x - 1)
