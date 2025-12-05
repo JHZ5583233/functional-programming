edbc19f6df1ff7208b8588c8f4116fea7f22b150
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-05 14:17:21 +0100

    very slow again

--- lab_2/primsiev.hs
+++ lab_2/primsiev.hs
@@ -1,9 +1,9 @@
 primes::[Integer]
-primes = 2 : [(2*x) + 1| x <- [1..], x `notElem` [i + j + (2*j*i)| i <- [1..x], j <- [i..x]]]
+primes = 2 : [(2*x) + 1| x <- [1..], noList x 1 1]
 
-noList :: Integer -> [Integer]
-noList x
-    | x == 1 = [i + x + (2*x*i)| i <- [1..x]]
-    | otherwise = ([i + x + (2*x*i)| i <- [1..x]]) ++ lastl
-        where
-            lastl = noList (x - 1)
+noList :: Integer -> Integer -> Integer -> Bool
+noList n i j
+  | (i+j+(2*i*j)) > n = True
+  | (i+j+(2*i*j)) == n = False
+  | otherwise = noList n (i + 1) j && noList n (i + 1) (j + 1) && noList n i (j + 1)
+
