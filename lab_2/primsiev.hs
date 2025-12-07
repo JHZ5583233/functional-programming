0f9feabd8543476fce30366bb145d8b0e569974a
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-07 05:34:45 +0100

    fsdf

--- lab_2/primsiev.hs
+++ lab_2/primsiev.hs
@@ -1,9 +1,12 @@
-primes::[Integer]
-primes = 2 : [(2*x) + 1| x <- [1..], noList x 1 1]
+primes :: [Integer]
+primes = 2 : [2*x + 1 | x <- [1..], noCheck x 1 1]
 
-noList :: Integer -> Integer -> Integer -> Bool
-noList n i j
-  | (i+j+(2*i*j)) > n = True
-  | (i+j+(2*i*j)) == n = False
-  | otherwise = noList n (i + 1) j && noList n (i + 1) (j + 1) && noList n i (j + 1)
-
+noCheck :: Integer -> Integer -> Integer -> Bool
+noCheck n i j
+  | i > n = True
+  | j > n = noCheck n (i + 1) (i + 1)
+  | y == n = False
+  | y > n = noCheck n (i + 1) (i + 1)
+  | otherwise = noCheck n i (j + 1)
+  where
+    y = i + j + 2*i*j
