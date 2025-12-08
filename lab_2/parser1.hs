e7bb15523beadd02bf6495b0ba77cdef305f6d38
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-08 15:09:22 +0100

    pars 1

--- lab_2/parser1.hs
+++ lab_2/parser1.hs
@@ -2,11 +2,17 @@ import Data.Char
 import Debug
 
 {- parser for the grammar:
-    S -> F S'
-    S' -> * F S'
-    S' -> / F S'
-    S' -> <empty string>
-    F -> <digits>
+    E -> T E’
+    E’ -> + T E’
+    E’ -> - T E’
+    E’ -> <empty string>
+    T -> F T’
+    T’ -> * F T’
+    T’ -> / F T’
+    T’ -> % F T’
+    T’ -> <empty string>
+    F -> ( E )
+    F -> <integer>
 -}
 
 type Token  = String
@@ -16,23 +22,53 @@ lexer :: String -> Tokens
 lexer [] = []
 lexer str@(c:cs)
   | c `elem` "\n\t "  = lexer cs -- skip whitespace
-  | c `elem` "*/+-%"  = [c]:lexer cs
-  | isDigit c    = takeWhile isDigit str : lexer (dropWhile isDigit str)
-  | otherwise    = abort $ printf "illegal character '%c' found." c
+  | c `elem` "*/+-%() " = [c]:lexer cs
+  | isDigit c = takeWhile isDigit str : lexer (dropWhile isDigit str)
+  | otherwise = abort $ printf "illegal character '%c' found." c
 
-parseS :: (String,Tokens) -> (String,Tokens)
-parseS = parseS'.parseF
+parseE :: Tokens -> (Integer, Tokens)
+parseE toks = parseE' v toks'
+  where
+    (v, toks') = parseT toks
 
-parseS' :: (String,Tokens) -> (String,Tokens)
-parseS' (acc,"*":toks) = parseS' $ parseF (acc++"*",toks)
-parseS' (acc,"/":toks) = parseS' $ parseF (acc++"/",toks)
-parseS' (acc,toks)     = (acc, toks)
+parseE' :: Integer -> Tokens -> (Integer, Tokens)
+parseE' acc ("+":toks) =
+  let (v, toks') = parseT toks
+  in parseE' (acc + v) toks'
+parseE' acc ("-":toks) =
+  let (v, toks') = parseT toks
+  in parseE' (acc - v) toks'
+parseE' acc toks = (acc, toks)
 
-parseF :: (String,Tokens) -> (String,Tokens)
-parseF (acc,[])      =  abort "error: unexpected end of input."
-parseF (acc,tok:toks)
-  | isDigit (head tok) =  (acc++tok, toks)
-  | otherwise          =  abort $ printf "Error, unexpected '%s'." tok
+parseT :: Tokens -> (Integer, Tokens)
+parseT toks = parseT' v toks'
+  where
+    (v, toks') = parseF toks
 
-parser :: String -> (String,Tokens)
-parser str = parseS ("", lexer str)
+parseT' :: Integer -> Tokens -> (Integer, Tokens)
+parseT' acc ("*":toks) =
+  let (v, toks') = parseF toks
+  in parseT' (acc * v) toks'
+parseT' acc ("/":toks) =
+  let (v, toks') = parseF toks
+  in parseT' (acc `div` v) toks'
+parseT' acc ("%":toks) =
+  let (v, toks') = parseF toks
+  in parseT' (acc `mod` v) toks'
+parseT' acc toks = (acc, toks)
+
+parseF :: Tokens -> (Integer, Tokens)
+parseF [] = abort "error: unexpected end of input."
+parseF ("(":toks) =
+  let (v, toks') = parseE toks
+  in case toks' of
+        (")":rest) -> (v, rest)
+        _ -> abort "error: expected ')'"
+parseF (tok:toks)
+  | all isDigit tok = (read tok, toks)
+  | otherwise = abort $ printf "Error, unexpected '%s'." tok
+
+evalParser :: String -> Integer
+evalParser str = case parseE (lexer str) of
+  (val, []) -> val
+  (_, _) -> abort "error: unexpected tokens after expression"
--- lab_2/primsiev.hs
+++ lab_2/primsiev.hs
@@ -1,5 +1,5 @@
 primes :: [Integer]
-primes = 2 : [2*x + 1 | x <- [1..], noCheck x 1 1]
+primes = 2 : [2*x + 1 | x <- [1..], noCheck2 x]
 
 noCheck :: Integer -> Integer -> Integer -> Bool
 noCheck n i j
@@ -10,3 +10,21 @@ noCheck n i j
   | otherwise = noCheck n i (j + 1)
   where
     y = i + j + 2*i*j
+
+noCheck2 :: Integer -> Bool
+noCheck2 n =
+  all (not . valid) oddDivs
+  where
+    m = 2*n + 1
+    oddDivs = [d | d <- divisors m, odd d]
+    valid a =
+      let b = m `div` a
+          i = (a - 1) `div` 2
+          j = (b - 1) `div` 2
+       in a <= b && a * b == m && i >= 1 && j >= i
+
+divisors :: Integer -> [Integer]
+divisors m =
+  concat [if d*d == m then [d] else [d, m `div` d]
+         | d <- [1 .. floor (sqrt (fromIntegral m))]
+         , m `mod` d == 0]
