25bd8f103a32690d905531540480ca20b3cfc232
JHZ5583233 <j.h.zhang@student.rug.nl>
2026-01-12 12:38:15 +0100

    adjaskhfkd

--- lab_3/Resolution.hs
+++ lab_3/Resolution.hs
@@ -41,8 +41,8 @@ applyRules (r:rs) f
         extractJust (Just x) = x
         extractJust Nothing = []
 
-applyAllFact :: Clause -> Clause -> Clauses
-applyAllFact r f = concat [applyRule r x | x <- fs]
+applyAllFact :: Clauses -> Clause -> Clauses
+applyAllFact rs f = concat (catMaybes [applyRule r x | r <- rs, x <- fs])
     where
         fs = makeAllFacts f
 
@@ -63,7 +63,7 @@ argIter (a:as) = [x : y | x <- [a, Arg (argName a ++ "x")], y <- argIter as]
 applyRule :: Clause -> Clause -> Maybe Clauses
 applyRule r f
     | null cs = Nothing
-    | length nonMatchingPairs > 1 = applyAllFact r f
+    | length nonMatchingPairs > 1 = Just (applyAllFact [r] f)
     | otherwise = Just result
     where
         queryFunc = head (extractFunc f)
