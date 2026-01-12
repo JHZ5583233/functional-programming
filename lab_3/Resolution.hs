5eff3b355f64ddb1e5bd57549a10f4e4b5eb1fa9
JHZ5583233 <j.h.zhang@student.rug.nl>
2026-01-12 16:47:05 +0100

    sdfsdfsd

--- lab_3/Resolution.hs
+++ lab_3/Resolution.hs
@@ -2,126 +2,60 @@ module Resolution (resolveClauses) where
 
 import Types
 import Unify
-import Data.Maybe (isNothing, catMaybes, isJust, fromMaybe)
-import Data.List (find)
+import Data.Maybe (isJust, fromMaybe)
 
 resolveClauses :: Clauses -> Clauses
 resolveClauses = fixpoint
   where
     fixpoint clauses =
-      let rs = [x | x <- clauses, length x > 1]
-          newClauses = removeDuplicates (map normalizeClause (clauses ++ resolveHelp rs clauses))
+      let rs = filter ((> 1) . length) clauses
+          newClauses = removeDuplicates $ map normalizeClause (clauses ++ resolveHelp rs clauses)
       in if length newClauses == length clauses
          then newClauses
          else fixpoint newClauses
 
-normalizeClause :: Clause -> Clause
-normalizeClause = removeDupLiterals
-  where
-    removeDupLiterals [] = []
-    removeDupLiterals (l:ls) = l : removeDupLiterals (filter (not . literalEqual l) ls)
+literalEqual :: (FuncApplication, Bool) -> (FuncApplication, Bool) -> Bool
+literalEqual (f1, p1) (f2, p2) = p1 == p2 && funcEqual f1 f2
 
-    literalEqual (f1, p1) (f2, p2) =
-        funcEqual f1 f2 && p1 == p2
-    funcEqual (FuncApp n1 args1) (FuncApp n2 args2) =
-        n1 == n2 && length args1 == length args2 &&
-        and (zipWith argEqual args1 args2)
-    argEqual (Const s1) (Const s2) = s1 == s2
-    argEqual (Arg s1) (Arg s2) = s1 == s2
-    argEqual _ _ = False
+funcEqual :: FuncApplication -> FuncApplication -> Bool
+funcEqual (FuncApp n1 args1) (FuncApp n2 args2) =
+    n1 == n2 && length args1 == length args2 && and (zipWith argEqual args1 args2)
+
+argEqual :: Argument -> Argument -> Bool
+argEqual (Const s1) (Const s2) = s1 == s2
+argEqual (Arg s1) (Arg s2) = s1 == s2
+argEqual _ _ = False
+
+normalizeClause :: Clause -> Clause
+normalizeClause [] = []
+normalizeClause (l:ls) = l : normalizeClause (filter (not . literalEqual l) ls)
+
+clausesEqual :: Clause -> Clause -> Bool
+clausesEqual a b = length a == length b && and (zipWith literalEqual a b)
 
 removeDuplicates :: Clauses -> Clauses
 removeDuplicates [] = []
 removeDuplicates (c:cs) = c : removeDuplicates (filter (not . clausesEqual c) cs)
-    where
-        clausesEqual a b =
-            length a == length b && and (zipWith literalEqual a b)
-        literalEqual (f1, p1) (f2, p2) =
-            funcEqual f1 f2 && p1 == p2
-        funcEqual (FuncApp n1 args1) (FuncApp n2 args2) =
-            n1 == n2 && length args1 == length args2 &&
-            and (zipWith argEqual args1 args2)
-        argEqual (Const s1) (Const s2) = s1 == s2
-        argEqual (Arg s1) (Arg s2) = s1 == s2
-        argEqual _ _ = False
 
 resolveHelp :: Clauses -> Clauses -> Clauses
-resolveHelp _ [] = []
-resolveHelp rs (f:fs) = cs ++ resolveHelp rs fs
-    where
-        cs = applyRules rs f
+resolveHelp rs = concatMap (applyRules rs)
 
 applyRules :: Clauses -> Clause -> Clauses
-applyRules [] _ = []
-applyRules (r:rs) f
-    | isNothing cs = applyRules rs f
-    | otherwise = extractJust cs ++ applyRules rs f
-    where
-        cs = applyRule r f
-        extractJust (Just x) = x
-        extractJust Nothing = []
-
-applyAllFact :: Clauses -> Clause -> Clauses
-applyAllFact rs f = concat (catMaybes [applyRuleAlt r x | r <- rs, x <- fs])
-    where
-        fs = makeAllFacts f
-
-makeAllFacts :: Clause -> Clauses
-makeAllFacts ((FuncApp ss as, b):cs) = [(FuncApp ss x, b) : cs | x <- fcoms]
-    where
-        coms = argIter as
-        fcoms = take (length coms - 1) coms
-
-argName :: Argument -> String
-argName (Const a) = a
-argName (Arg a) = a
-
-argIter :: [Argument] -> [[Argument]]
-argIter [] = [[]]
-argIter (a:as) = [x : y | x <- [a, Arg (argName a ++ "x")], y <- argIter as]
+applyRules rs f = concatMap (\r -> maybe [] id (applyRule r f)) rs
 
 applyRule :: Clause -> Clause -> Maybe Clauses
-applyRule r f
-    | null cs = Nothing
-    | otherwise = Just results
+applyRule r f@((fFunc, fPol):_)
+    | null matches = Nothing
+    | otherwise = Just [resolveAt idx unifier | (idx, unifier) <- matches]
     where
-        (fFunc, fPol) = head f
         matches = [(i, u) | (i, (rFunc, rPol)) <- zip [0..] r,
                             rPol /= fPol,
                             let u = mgu fFunc rFunc,
                             isJust u]
-
-        cs = matches
-
-        results = [resolveAt matchIdx unifier | (matchIdx, unifier) <- matches]
 
         resolveAt matchIdx unifier =
-            let unifiedSubsts = fromMaybe [] unifier
-                remainingFromF = [(applyUnifier unifiedSubsts func, pol) | (func, pol) <- tail f]
-                remainingFromR = [(applyUnifier unifiedSubsts func, pol) | (i, (func, pol)) <- zip [0..] r, i /= matchIdx]
+            let substs = fromMaybe [] unifier
+                remainingFromF = [(applyUnifier substs func, pol) | (func, pol) <- tail f]
+                remainingFromR = [(applyUnifier substs func, pol) | (i, (func, pol)) <- zip [0..] r, i /= matchIdx]
             in remainingFromF ++ remainingFromR
-
-applyRuleAlt :: Clause -> Clause -> Maybe Clauses
-applyRuleAlt r f
-    | null cs = Nothing
-    | otherwise = Just results
-    where
-        (fFunc, fPol) = head f
-        matches = [(i, u) | (i, (rFunc, rPol)) <- zip [0..] r,
-                            rPol /= fPol,
-                            let u = mgu fFunc rFunc,
-                            isJust u]
-
-        cs = matches
-        (matchIdx, unifier) = head matches
-        unifiedSubsts = fromMaybe [] unifier
-
-        remainingFromR = [(applyUnifier unifiedSubsts func, pol) | (i, (func, pol)) <- zip [0..] r, i /= matchIdx]
-
-        remainingFromF = [(applyUnifier unifiedSubsts func, pol) | (func, pol) <- tail f]
-
-        results = [remainingFromR ++ remainingFromF]
-
-extractFunc :: Clause -> [FuncApplication]
-extractFunc [] = []
-extractFunc cs = [x | (x, _) <- cs]
+applyRule _ [] = Nothing
