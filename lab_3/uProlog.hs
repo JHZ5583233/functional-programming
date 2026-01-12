7fb834eb8d651855b7ee2fdf368cb4df70a7b197
JHZ5583233 <j.h.zhang@student.rug.nl>
2026-01-12 15:35:56 +0100

    f shfks

--- lab_3/uProlog.hs
+++ lab_3/uProlog.hs
@@ -9,8 +9,25 @@ import Resolution
 process :: String -> Clauses
 process = resolveClauses.programToClauses.analyse.parseProgram
 
+-- Pretty print a single literal (FuncApplication, Bool)
+prettyLiteral :: (FuncApplication, Bool) -> String
+prettyLiteral (func, True) = show func
+prettyLiteral (func, False) = "¬" ++ show func
+
+-- Pretty print a clause (disjunction of literals)
+prettyClause :: Clause -> String
+prettyClause [] = "□"  -- Empty clause (contradiction)
+prettyClause [lit] = prettyLiteral lit
+prettyClause lits = unwords [prettyLiteral lit ++ (if i < length lits - 1 then " ∨" else "") | (lit, i) <- zip lits [0..]]
+
+-- Pretty print all clauses
+prettyClauses :: Clauses -> String
+prettyClauses cs = unlines $ zipWith (\i c -> show i ++ ". " ++ prettyClause c) [1..] cs
+
 main = do
   args <- getArgs
   let reader = if null args then getContents else readFile (head args)
   text <- reader
-  print (process text)
+  putStrLn "\n=== Results ==="
+  putStrLn (prettyClauses (process text))
+  putStrLn ("Total clauses: " ++ show (length (process text)))
