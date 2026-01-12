99bb7d1e35ea4952685de0f21894f5b7a6b6c520
JHZ5583233 <j.h.zhang@student.rug.nl>
2026-01-12 17:11:10 +0100

    yes

--- lab_3/Query.hs
+++ lab_3/Query.hs
@@ -3,7 +3,7 @@ module Query (answerQueries) where
 import Types
 
 answerQueries :: Program -> String
-answerQueries [] = ""
-answerQueries ((Fact f, _):rs) = "/n" ++ answerQueries rs
-answerQueries ((Query q, _):rs) = "/n" ++ answerQueries rs
-answerQueries ((Rule ru rus, _):rs) = "/n" ++ answerQueries rs
+answerQueries (Program []) = ""
+answerQueries (Program ((Fact f, _):rs)) = "/n" ++ answerQueries (Program rs)
+answerQueries (Program ((Query q, _):rs)) = "/n" ++ answerQueries (Program rs)
+answerQueries (Program ((Rule ru rus, _):rs)) = "/n" ++ answerQueries (Program rs)
